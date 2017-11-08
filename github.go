49e49ef16fadff029954b89c94c30a5f1bfa0c84
Simon Tucker <brotherlogic@gmail.com>
2017-11-07 19:50:16 -0800

    Added first loggging

--- github.go
+++ github.go
@@ -134,6 +134,8 @@ func (b *GithubBridge) issueExists(title string) (*pbgh.Issue, error) {
 	urlv := "https://api.github.com/user/issues"
 	body, err := b.visitURL(urlv)
 
+	b.Log(fmt.Sprintf("Checked %v -> %v", err, string(body)))
+
 	if err != nil {
 		return nil, err
 	}
