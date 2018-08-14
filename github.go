cdba08b9ab45a0c3b2aecf15229d288097cfdada
Simon Tucker <brotherlogic@gmail.com>
2018-08-14 15:08:48 -0700

    Fixes githubcard

--- github.go
+++ github.go
@@ -65,7 +65,7 @@ func (b GithubBridge) ReportHealth() bool {
 }
 
 // Mote promotes this server
-func (b GithubBridge) Mote(master bool) error {
+func (b GithubBridge) Mote(ctx context.Context, master bool) error {
 	return nil
 }
 
@@ -321,9 +321,9 @@ func main() {
 	b.RegisterServer("githubcard", false)
 
 	if len(*token) > 0 {
-		b.Save("/github.com/brotherlogic/githubcard/token", &pbgh.Token{Token: *token})
+		b.Save(context.Background(), "/github.com/brotherlogic/githubcard/token", &pbgh.Token{Token: *token})
 	} else {
-		m, _, err := b.Read("/github.com/brotherlogic/githubcard/token", &pbgh.Token{})
+		m, _, err := b.Read(context.Background(), "/github.com/brotherlogic/githubcard/token", &pbgh.Token{})
 		if err != nil {
 			log.Printf("Failed to read token: %v", err)
 		} else {
